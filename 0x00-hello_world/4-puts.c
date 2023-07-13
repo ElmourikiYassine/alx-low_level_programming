b5d1f2617ce069f78f69eae3363946c473988c97
ElmourikiYassine <yassineelmouriki@gmail.com>
2023-07-13 05:29:58 -0700

    task_4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - This is the entry
+ * Return: - if everything alright
+ */
+int main(void)
+{
+	char sen[200] = "\"Programming is like building a multilingual puzzle";
+
+	puts(sen);
+	return (0);
+}
