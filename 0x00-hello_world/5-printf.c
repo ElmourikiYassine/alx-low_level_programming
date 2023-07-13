aba38332294e9935bd1a00297f3521a809d59438
ElmourikiYassine <yassineelmouriki@gmail.com>
2023-07-13 06:38:35 -0700

    task_5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+* main - this is the entry
+* Return: 1 if no error accured
+*/
+int main(void)
+{
+	char sen[200] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", sen);
+	return (0);
+}
