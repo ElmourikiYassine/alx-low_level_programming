5b79f1ae3542c14bf80fdadcc194ec91ee99c3e2
ElmourikiYassine <yassineelmouriki@gmail.com>
2023-07-14 05:31:55 -0700

    task_5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - programme entry
+ * Return: 0 if no error
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+	return (0);
+}
