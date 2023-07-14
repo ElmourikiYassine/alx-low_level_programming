20e030b7a969aae518a359833b5ad1e31e9d738a
ElmourikiYassine <yassineelmouriki@gmail.com>
2023-07-14 05:35:33 -0700

    task_6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - programme entry
+ * Return: 0 if no error
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
