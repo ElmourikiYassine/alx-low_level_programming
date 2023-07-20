a6490019c4e9d7581b6889418779d9b25b8e8b71
ElmourikiYassine <yassineelmouriki@gmail.com>
2023-07-20 03:32:15 -0700

    task_5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,14 +6,16 @@
 
 void more_numbers(void)
 {
-	int i;
+	int i, j;
 
-	for (i = 0; i <= 14; i++)
+	for (j = 0; j < 10; j++)
 	{
-		if ((i / 10) != 0)
-			_putchar(i / 10 + '0');
-		_putchar(i % 10 + '0');
+		for (i = 0; i <= 14; i++)
+		{
+			if ((i / 10) != 0)
+				_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
-
 }
