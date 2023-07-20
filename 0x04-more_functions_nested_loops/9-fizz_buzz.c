ffe41d080ac04b2d79d084a0edb7d3c708712870
ElmourikiYassine <yassineelmouriki@gmail.com>
2023-07-20 05:27:41 -0700

    task_9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (!(i % 3) && !(i % 5))
+			printf("FizzBuzz");
+		else if (!(i % 3))
+			printf("Fizz");
+		else if (!(i % 5))
+			printf("Buzz");
+		else
+			printf("%d", i);
+		if (i != 100)
+			putchar(' ');
+	}
+	printf("\n");
+	return (0);
+}
