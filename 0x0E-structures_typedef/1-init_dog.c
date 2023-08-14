b6fbfe796f10c0ed5f4f25d22b41d091bb3a6496
ElmourikiYassine <yassineelmouriki@gmail.com>
2023-08-14 03:44:03 -0700

    task_1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+/**
+ * init_dog - check the code
+ *
+ * @d: the pointer to the struct.
+ * @name: the name to be assigned to the struct field.
+ * @age: the age to be assigned to the struct field.
+ * @owner: the owner to be assigned to the struct field.
+ *
+ * Return: nothing.
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
