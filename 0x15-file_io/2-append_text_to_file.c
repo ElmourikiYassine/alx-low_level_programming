6ed9b3ea5f12e1a873dca44cf1dbaff6ee93bd55
ElmlourikiYassine <yassineelmouriki@gmail.com>
2023-09-04 08:40:03 -0700

    task_0x15_2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,66 @@
+#include "main.h"
+
+/**
+ * _len - computes the length of a string
+ * @str: the string to compute the length of
+ *
+ * Return: the length of the string
+ */
+int _len(char *str);
+
+/**
+ * append_text_to_file - appends text to an existing file
+ * @filename: the name of the file to append to
+ * @text_content: the content to append to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes_write;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_RDWR | O_APPEND, S_IRUSR | S_IWUSR);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
+
+	bytes_write = write(fd, text_content, _len(text_content));
+
+	if (bytes_write == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
+
+/**
+ * _len - computes the length of a string
+ * @str: the string to compute the length of
+ *
+ * Return: the length of the string
+ */
+int _len(char *str)
+{
+	char *c = str;
+	int i = 0;
+
+	while (*c != '\0')
+	{
+		i++;
+		c++;
+	}
+
+	return (i);
+}
+
