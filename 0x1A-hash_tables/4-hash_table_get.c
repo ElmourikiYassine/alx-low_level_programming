2d376083bb1b2a29df371916993015005e6e41ed
ElmourikiYassine <yassineelmouriki@gmail.com>
2023-11-03 10:00:40 -0700

    0x1A_task_4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,14 +10,21 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int index = key_index((unsigned char *)key, ht->size);
-	hash_node_t *current = ht->array[index];
+	unsigned long int index;
+	hash_node_t *current_node;
 
-	while (current)
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	index = key_index((unsigned char *)key, ht->size);
+
+	current_node = ht->array[index];
+
+	while (current_node)
 	{
-		if (!strcmp(current->key, key))
-			return (current->value);
-		current = current->next;
+		if (strcmp(current_node->key, key) == 0)
+			return (current_node->value);
+		current_node = current_node->next;
 	}
 
 	return (NULL);
