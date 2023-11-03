e224aa107d4bdd45e9f8a756dc0aadc79101fc07
ElmourikiYassine <yassineelmouriki@gmail.com>
2023-11-03 09:56:21 -0700

    0x1A_task_3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,42 +11,44 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int ht_size = ht->size;
-	unsigned long int index = key_index((const unsigned char *) key, ht_size);
-	hash_node_t *new_hash_node, *current;
+	unsigned long int index;
+	hash_node_t *new_node, *current_node;
 
-	current = ht->array[index];
-	while (current)
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	index = key_index((unsigned char *)key, ht->size);
+	current_node = ht->array[index];
+	while (current_node)
 	{
-		if (!strcmp(current->key, key))
+		if (strcmp(current_node->key, key) == 0)
 		{
-			free(current->value);
-			current->value = strdup(value);
-			if (!current->value)
+			free(current_node->value);
+			current_node->value = strdup(value);
+			if (current_node->value == NULL)
 				return (0);
 			return (1);
 		}
-		current = current->next;
+		current_node = current_node->next;
 	}
-	new_hash_node = malloc(sizeof(hash_node_t));
-	if (!new_hash_node)
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
 		return (0);
 
-	new_hash_node->key = strdup(key);
-	new_hash_node->value = strdup(value);
-	new_hash_node->next = NULL;
-	if (!new_hash_node->key)
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
 	{
-		free(new_hash_node);
+		free(new_node);
 		return (0);
 	}
-	if (!new_hash_node->value)
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
 	{
-		free(new_hash_node->key);
-		free(new_hash_node);
+		free(new_node->key);
+		free(new_node);
 		return (0);
 	}
-	new_hash_node->next = ht->array[index];
-	ht->array[index] = new_hash_node;
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
 	return (1);
 }
